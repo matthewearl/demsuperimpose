0fe2f08e239b0b6ba007d7c55e872a5599d62acf
Matthew Earl <git@matthewearl.com>
2025-04-09 08:46:14 +0100

    installable package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='demsuperimpose',
+    description='Quake demo superimposer',
+    url='https://github.com/matthewearl/demsuperimpose',
+    author='Matthew Earl',
+    packages=['demsuperimpose'],
+    install_requires=[],
+)
